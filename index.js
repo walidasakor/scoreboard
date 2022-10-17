da5fef5433514fc001188badeda0ff817e643b85
walidasakor <111765419+walidasakor@users.noreply.github.com>
2022-10-17 01:29:37 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,34 @@
+let count = 0
+let counta=0
+
+let countEl= document.getElementById("count-el")
+
+function plusone(){
+    count= count+1
+    countEl.textContent=count
+}
+
+function plustwo(){
+    count+=2
+    countEl.textContent=count
+}
+
+function plusthree(){
+    count+=3
+    countEl.textContent=count
+}
+
+let countEL1= document.getElementById("count-ela")
+
+function plusonea(){
+    counta+=1
+    countEL1.textContent=counta
+}
+function plustwoa(){
+    counta+=2
+    countEL1.textContent=counta
+}
+function plusthreea(){
+    counta+=3
+    countEL1.textContent=counta
+}
